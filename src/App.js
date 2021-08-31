9ab9e9d270aaead9896aea4fa299d6a212f72237
MohitIooLabs <mohit.sahu@ioolabs.com>
2021-08-31 15:43:59 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import Posts from "./components/posts";
+import './App.css';
+
+const store = configureStore();
+
+function App() {
+  return (
+    <Provider store={store}>
+            <Posts />
+        </Provider>
+  );
+}
+
+export default App;
